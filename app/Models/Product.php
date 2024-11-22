25024c12c56c92deb347e80bbed42e2c3472521e
Saman Babaei <alibabaeian670@gmail.com>
2024-11-22 19:08:01 +0330

    inject observer

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use App\Observers\ProductObserver;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy([ProductObserver::class])]
 class Product extends Model
 {
     protected $fillable = ['title','price','stock'];
